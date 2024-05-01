ec71b6e9e84c45d4a559ab8480c452db4e27c9dc
Arty <shrisharvesh7@gmail.com>
2024-05-01 23:17:14 +0800

    Refactor pat.py to improve readability and add support for additional variations of "pat me" command

--- pat.py
+++ pat.py
@@ -8,27 +8,38 @@ load_dotenv()
 
 CHANNEL = os.getenv("CHANNEL")
 TOKEN = os.getenv("TOKEN")
-DISALLOWED_IDS = [int(id) for id in os.getenv("DISALLOWED_IDS").split(",")] if os.getenv("DISALLOWED_IDS") else []
-REGEX = re.compile(r"(?i)((pls|plz|plis|please) )?pa+t me+( (pls|plz|plis|please))?")
+DISALLOWED_IDS = (
+    [int(id) for id in os.getenv("DISALLOWED_IDS").split(",")]
+    if os.getenv("DISALLOWED_IDS")
+    else []
+)
+REGEX = re.compile(
+    r"(?i)((pls|plz|plis|ple+ase|pwease|Pwe+ase|plez|ple+ez|ple+ase) )?(pa+t|pwat) (me|mwe)+( (pls|plz|plis|ple+ase|Pwease|Pwe+ase|plez|ple+ez|ple+ase))?"
+)
 
 intents = discord.Intents.default()
-intents.messages = True  # This allows the bot to receive events about messages being sent and edited
-intents.message_content = True 
+intents.messages = (
+    True  # This allows the bot to receive events about messages being sent and edited
+)
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print('Logged on as', client.user)
+    print("Logged on as", client.user)
+
 
 @client.event
 async def on_message(message):
-        if not REGEX.match(message.content):
-            if message.author.id in DISALLOWED_IDS:
-                await message.delete()
-        else:
-            await message.channel.send(f"{message.author.mention} *pats*")
-            print(f"Bot activated by {message.author} at {message.created_at}")
+    if not REGEX.match(message.content):
+        if message.author.id in DISALLOWED_IDS:
+            await message.delete()
+    else:
+        await message.channel.send(f"{message.author.mention} *pats*")
+        print(f"Bot activated by {message.author} at {message.created_at}")
+
 
 @client.event
 async def on_message_edit(before, after):
@@ -38,5 +49,6 @@ async def on_message_edit(before, after):
                 await after.delete()
             else:
                 print(f"Bot activated by {after.author} at {after.edited_at}")
+
 
 client.run(TOKEN)
